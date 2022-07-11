7ac7a964725632930c103095a6eda6f38b06e7d0
Christopher Sinlasath <csinlasath@live.com>
2022-07-11 17:40:45 -0500

    First 10

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,5 +1,7 @@
 const leaderboardListEl = document.getElementById("leadboard-list");
 
+const MAX_NUM_SCORES_TO_SHOW = 10;
+
 function getExistingScores() {
   let leaderboard = JSON.parse(localStorage.getItem("leaderboard"));
 
@@ -10,22 +12,39 @@ function getExistingScores() {
   return leaderboard;
 }
 
+function sortDecending(arr) {
+  const toReturn = arr;
+
+  toReturn.sort(function (a, b) {
+    return b.percentage - a.percentage;
+  });
+
+  return toReturn;
+}
+
 function changeFloatToPercentage(num) {
-  switch (num) {
-    case 1:
-        return "100%"
-    case 0:
-        return "0%"
+  switch (parseFloat(num)) {
+    case 1.00:
+      return "100%";
+    case 0.00:
+      return "0%";
     default:
       return num.toString().slice(num.indexOf(".") + 1) + "%";
   }
 }
 
 function renderListItem(scores) {
-  for (let i = 0; i < scores.length; i++) {
-    const score = scores[i];
+  let sortedScores = sortDecending(scores);
+  if (sortedScores.length > MAX_NUM_SCORES_TO_SHOW) {
+    sortedScores = sortedScores.slice(0, MAX_NUM_SCORES_TO_SHOW - 1);
+  }
+  
+  for (let i = 0; i < sortedScores.length; i++) {
+    const score = sortedScores[i];
     const newScoreEl = document.createElement("li");
-    newScoreEl.textContent = `${score.playerName}: ${changeFloatToPercentage(score.percentage)}`;
+    newScoreEl.textContent = `${score.playerName}: ${changeFloatToPercentage(
+      score.percentage
+    )}`;
 
     leaderboardListEl.append(newScoreEl);
   }
